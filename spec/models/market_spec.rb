56616455356ed749d0f9ebf16dc7200b256b265b
Matthew Lim <matlim200@gmail.com>
2023-08-29 16:39:31 -0700

    feat: add validations and validation tests

--- spec/models/market_spec.rb
+++ spec/models/market_spec.rb
@@ -5,4 +5,49 @@ RSpec.describe Market, type: :model do
     it { should have_many(:market_vendors) }
     it { should have_many(:vendors).through(:market_vendors) }
   end
+
+  describe "#validations" do
+    it "is valid with valid attributes" do
+      market = create(:market)
+      expect(market).to be_valid
+    end
+
+    it "is not valid without a name" do
+      market = Market.new(name: nil)
+      expect(market).to_not be_valid
+    end
+    it "is not valid without a street" do
+      market = Market.new(street: nil)
+      expect(market).to_not be_valid
+    end
+    it "is not valid without a city" do
+      market = Market.new(city: nil)
+      expect(market).to_not be_valid
+    end
+    it "is not valid without a state" do
+      market = Market.new(state: nil)
+      expect(market).to_not be_valid
+    end
+    it "is not valid without a zip" do
+      market = Market.new(zip: nil)
+      expect(market).to_not be_valid
+    end
+  end
+
+  describe "#vendor_count" do
+    it "counts the number of vendor a market has" do
+      @g_markets = create_list(:market, 3)
+
+      @g_vendors = create_list(:vendor, 9)
+
+      @g_markets.each do |market|
+        3.times do
+          vendor = @g_vendors.sample
+          create(:market_vendor, market: market, vendor: vendor)
+        end
+      end
+
+      expect(@g_markets.first.vendor_count).to eq(3)
+    end
+  end
 end
--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -5,4 +5,28 @@ RSpec.describe Vendor, type: :model do
     it { should have_many(:market_vendors) }
     it { should have_many(:markets).through(:market_vendors) }
   end
+
+  describe "#validations" do
+    it "is valid with valid attributes" do
+      vendor = create(:vendor)
+      expect(vendor).to be_valid
+    end
+
+    it "is not valid without a name" do
+      vendor = Vendor.new(name: nil)
+      expect(vendor).to_not be_valid
+    end
+    it "is not valid without a contact_name" do
+      vendor = Vendor.new(contact_name: nil)
+      expect(vendor).to_not be_valid
+    end
+    it "is not valid without a contact_phone" do
+      vendor = Vendor.new(contact_phone: nil)
+      expect(vendor).to_not be_valid
+    end
+    it "is not valid without a credit_accepted" do
+      vendor = Vendor.new(credit_accepted: nil)
+      expect(vendor).to_not be_valid
+    end
+  end
 end
