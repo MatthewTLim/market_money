3d36923df655f4fbfaa4ecbcce7a869c27e3d200
Matthew Lim <matlim200@gmail.com>
2023-08-31 14:29:03 -0700

    feat: add route for destroy

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
         resources :vendors, only: [:index]
       end
       resources :vendors, only: [:show, :create, :update, :destroy]
-      resources :market_vendors, only: [:create]
+      resource :market_vendors, only: [:create, :destroy]
     end
   end
 end
