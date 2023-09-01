1976726656ebaefd610b6a3ff22a73359b102193
Matthew Lim <matlim200@gmail.com>
2023-09-01 08:24:33 -0700

    feat: add route for search markets

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
+      get "/markets/search", to: "markets#search"
       resources :markets, only: [:index, :show] do
         resources :vendors, only: [:index]
       end
+
       resources :vendors, only: [:show, :create, :update, :destroy]
       resource :market_vendors, only: [:create, :destroy]
     end
