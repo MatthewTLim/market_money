82ad7b9a108f51db126686284be702d99340fdd4
Matthew Lim <matlim200@gmail.com>
2023-09-01 08:25:03 -0700

    feat: add search action for market

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -9,5 +9,31 @@ class Api::V0::MarketsController < ApplicationController
     rescue ActiveRecord::RecordNotFound => e
     render json: { errors: [{ detail: e.message }] }, status: :not_found
   end
+
+  def search
+    city = params[:city]
+    state = params[:state]
+    name = params[:name]
+
+    if invalid_parameters?(city, state, name)
+      render json: { errors: [{ detail: "Invalid combination of parameters." }] }, status: :unprocessable_entity
+      return
+    end
+
+    @markets = Market.all
+
+    @markets = @markets.where("LOWER(name) = ?", name.downcase) if name.present?
+    @markets = @markets.where("LOWER(state) = ?", state.downcase) if state.present?
+    @markets = @markets.where("LOWER(city) = ?", city.downcase) if city.present?
+
+    render json: MarketSerializer.new(@markets)
+  end
+
+
+  private
+
+  def invalid_parameters?(city, state, name)
+    (city.present?) && (state.blank? && name.blank?) || (city.present? && name.present?) && state.blank? || (state.blank? && city.blank? && name.blank?)
+  end
 end
 
